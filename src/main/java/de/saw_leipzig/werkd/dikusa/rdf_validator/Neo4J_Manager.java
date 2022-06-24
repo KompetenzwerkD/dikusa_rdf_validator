2ac98d040428874b1f36a24baeeee166ec61be97
dgoldhahn <dgoldhahn@informatik.uni-leipzig.de>
2022-06-24 10:30:02 +0200

    added constructor

--- src/main/java/de/saw_leipzig/werkd/dikusa/rdf_validator/Neo4J_Manager.java
+++ src/main/java/de/saw_leipzig/werkd/dikusa/rdf_validator/Neo4J_Manager.java
@@ -1,1 +1,17 @@
+package de.saw_leipzig.werkd.dikusa.rdf_validator;
 
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Result;
+import org.neo4j.driver.Session;
+
+import static org.neo4j.driver.Values.parameters;
+
+public class Neo4J_Manager {
+	
+	public void initialize() {
+				
+	}
+	
+}
